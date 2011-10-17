995d1a00d28807758ae5cea9ed034cc727d118cb
Brian Dunn <brianpatrickdunn@gmail.com>
2011-10-16 22:04:40 -0500

    Move a spec to a feature

--- features/day.feature
+++ features/day.feature
@@ -17,3 +17,20 @@ Feature: day summary
       | 0.25  | @clacker |
       | 16.00 | @off     |
 
+  Scenario: multiple projects CSV
+    Given this project file:
+    """
+    Fri Jun 24 09:00:00 CDT 2011: @internal standup
+    Fri Jun 24 09:05:00 CDT 2011: @mulu-demo
+    Fri Jun 24 11:25:00 CDT 2011: @off lunch
+    Fri Jun 24 12:25:00 CDT 2011: @mulu
+    Fri Jun 24 17:25:00 CDT 2011: @off outie
+    """
+    When I clack with the arguments:
+      | day | 2011-06-24 |
+    Then I see this CSV:
+        | hours | project    | notes        |
+        | 0.08  | @internal  | standup      |
+        | 2.33  | @mulu-demo |              |
+        | 7.58  | @off       | lunch\noutie |
+        | 5.00  | @mulu      |              |
--- spec/integration/reports_time_spent_on_each_project_spec.rb
+++ spec/integration/reports_time_spent_on_each_project_spec.rb
@@ -5,39 +5,3 @@ describe 'Given a time file with zero entries on the given day' do
     it { should eq [[ 'hours', 'project', 'notes' ]] }
   end
 end
-
-describe 'Given a time file with one entry on the given day' do
-  it_should_behave_like :a_report do
-    let :yaml do
-      'Fri Jun 24 17:30:00 CDT 2011: @off outie'
-    end
-    it do
-      should eq [
-        [ 'hours', 'project', 'notes' ],
-        [ '6.50' , '@off'   , 'outie' ]
-      ]
-    end
-  end
-end
-describe 'Given a time file with multiple project tags', focus: true do
-  it_should_behave_like :a_report do
-    let :yaml do
-      <<-YAML
-        Fri Jun 24 09:00:00 CDT 2011: @internal standup
-        Fri Jun 24 09:05:00 CDT 2011: @mulu-demo
-        Fri Jun 24 11:25:00 CDT 2011: @off lunch
-        Fri Jun 24 12:25:00 CDT 2011: @mulu
-        Fri Jun 24 17:25:00 CDT 2011: @off outie
-      YAML
-    end
-    it do 
-      should eq [
-        [ 'hours', 'project'   , 'notes'   ],
-        [ '0.08' , '@internal' , 'standup' ],
-        [ '2.33' , '@mulu-demo', ''        ],
-        [ '7.58' , '@off'      , "lunch\noutie"   ],
-        [ '5.00' , '@mulu'     , ''        ]
-      ]
-    end
-  end
-end
