88aa131f84861d724be867b5c2a67633d6f3f37a
Brian Dunn <brianpatrickdunn@gmail.com>
2011-10-20 14:56:13 -0500

    break out cli to its own file

--- lib/clacker.rb
+++ lib/clacker.rb
@@ -3,10 +3,10 @@ require 'yaml'
 require 'date'
 require 'csv'
 require 'core_ext/array'
-require 'grit'
 require 'clacker/harvest'
 
 module Clacker
+  autoload :CLI, 'clacker/cli'
 
   def self.project= project
     @@project = project
@@ -15,125 +15,75 @@ module Clacker
   def self.project
     @@project
   end
+  class Entry < Struct.new :time, :duration, :text
 
-  class CLI < Thor
-
-    desc "[PROJECT_FILE] [DATE]", 'report about entries on the date'
-    method_options harvest: :boolean
-    def day project_file_path, date
-      @start = Date.parse(date)
-      @stop = Date.parse(date) + 1
-      Clacker.project = project_file = ProjectFile.new(project_file_path)
-      @open_entries = project_file.entries.map do |time, note|
-        [ DateTime.parse(time).to_time, note ]
-      end.select do |time, note|
-        @start.to_time <= time && @stop.to_time >= time
-      end
-      Harvest.push entries if options['harvest']
-      print_report entries
+    def note
+      note = Note.new(time, text)
     end
 
-    class Entry < Struct.new :time, :duration, :text
-
-      def note
-        note = Note.new(time, text)
-      end
-
-      def project_name
-        note.project_name
-      end
-
-      def other
-        note.other
-      end
+    def project_name
+      note.project_name
     end
 
-    class Note < Struct.new :time, :text
-      NAME_TAG = /(@[^\s]+)/
-      def project_name
-        text =~ NAME_TAG
-        $1
-      end
-      def other
-        text.gsub(NAME_TAG, '').strip + commit_messages
-      end
-      def date
-        time.to_date
-      end
-      def commit_messages
-        (commits if repo_path) || ''
-      end
-      def commits
-        `cd #{repo_path} && git log --pretty=%s --after #{date}T00:00 --before #{date}T23:59`.chomp
-      end
-      def repo_path
-        Clacker.project.repo_path(project_name)
-      end
+    def other
+      note.other
     end
 
-    class ProjectFile
+    def project_settings
+      Clacker.project.projects[project_name]
+    end
+  end
 
-      attr_reader :path
+  class Note < Struct.new :time, :text
+    NAME_TAG = /(@[^\s]+)/
+    def project_name
+      text =~ NAME_TAG
+      $1
+    end
+    def other
+      text.gsub(NAME_TAG, '').strip + commit_messages
+    end
+    def date
+      time.to_date
+    end
+    def commit_messages
+      (commits if repo_path) || ''
+    end
+    def commits
+      `cd #{repo_path} && git log --pretty=%s --after #{date}T00:00 --before #{date}T23:59`.chomp
+    end
+    def repo_path
+      Clacker.project.repo_path(project_name)
+    end
+  end
 
-      def initialize path
-        @path = path
-      end
+  class ProjectFile
 
-      def repo_path(project)
-        if project = projects[project]
-          project['path']
-        end
-      end
+    attr_reader :path
 
-      def entries
-        @entries ||= data.reject { |key, value| key =~ /^@/ }
-      end
-
-      def projects
-        @projects ||= data.reject { |key,value| key !~ /^@/ }
-      end
-
-      private
-
-      def data
-        @data ||= (YAML.load(File.read(path)) || {})
-      end
+    def initialize path
+      @path = path
     end
 
-    private
-
-    def print_report entries
-      CSV.generate do |csv|
-        csv << column_names
-        entries.group_by(&:project_name).map do |project, entries|
-          csv << [format( '%0.2f', entries.sum(&:duration) ), project, entries.map(&:other).uniq.join("\n")]
-        end
-      end.tap do |report|
-        puts report
+    def repo_path(project)
+      if project = projects[project]
+        project['path']
       end
     end
 
     def entries
-      [].tap do |entries|
-        @open_entries.each_with_index do |(time, note), i|
-        entries << Entry.new(time, hours_between(time, next_time(i)), note)
-        end
-      end
-    end
-    def hours_between start, stop
-      ( stop - start ) / 3600
+      @entries ||= data.reject { |key, value| key =~ /^@/ }
     end
 
-    def next_time i
-      if entry = @open_entries[i + 1]
-        entry.first
-      else
-        @stop.to_time
-      end
+    def projects
+      @projects ||= data.reject { |key,value| key !~ /^@/ }
     end
 
-    def column_names
-      %w[hours project notes]
+    private
+
+    def data
+      @data ||= (YAML.load(File.read(path)) || {})
     end
   end
+
 end
---
+++ lib/clacker/cli.rb
@@ -0,0 +1,55 @@
+module Clacker
+  class CLI < Thor
+
+    desc "[PROJECT_FILE] [DATE]", 'report about entries on the date'
+    method_options harvest: :boolean
+    def day project_file_path, date
+      @start = Date.parse(date)
+      @stop = Date.parse(date) + 1
+      Clacker.project = project_file = ProjectFile.new(project_file_path)
+      @open_entries = project_file.entries.map do |time, note|
+        [ DateTime.parse(time).to_time, note ]
+      end.select do |time, note|
+        @start.to_time <= time && @stop.to_time >= time
+      end
+      Harvest.push entries if options['harvest']
+      print_report entries
+    end
+
+    private
+
+    def print_report entries
+      CSV.generate do |csv|
+        csv << column_names
+        entries.group_by(&:project_name).map do |project, entries|
+          csv << [format( '%0.2f', entries.sum(&:duration) ), project, entries.map(&:other).uniq.join("\n")]
+        end
+      end.tap do |report|
+        puts report
+      end
+    end
+
+    def entries
+      [].tap do |entries|
+        @open_entries.each_with_index do |(time, note), i|
+        entries << Entry.new(time, hours_between(time, next_time(i)), note)
+        end
+      end
+    end
+    def hours_between start, stop
+      ( stop - start ) / 3600
+    end
+
+    def next_time i
+      if entry = @open_entries[i + 1]
+        entry.first
+      else
+        @stop.to_time
+      end
+    end
+
+    def column_names
+      %w[hours project notes]
+    end
+  end
+end
